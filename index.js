1de01677bfedbf06e81d6bf2576eb779ca7c652f
Nick Sarafa <nicksarafa@gmail.com>
2018-08-27 08:06:42 +0800

    update slack button

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ let UserModel = require("./models/User");
 // serve index.html to client
 app.get("/", (req, res) => {
   res.send(
-    '<a href="/auth/slack"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>'
+    '<a href="https://slack.com/oauth/authorize?client_id=346952315347.420991484773&scope=commands,bot"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>'
   );
 });
 
