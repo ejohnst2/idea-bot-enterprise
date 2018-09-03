ee27fb5a8696c6dd1039887dbd2ca9631da9cb84
Nick Sarafa <nicksarafa@gmail.com>
2018-09-03 15:45:29 +0800

    vuetify init

--- client/src/components/Ideas.vue
+++ client/src/components/Ideas.vue
@@ -1,40 +1,59 @@
 <template>
-  <div class="ideas">
-    <div v-for="idea in ideas" :key="idea._id">
-      <div class="idea">
-        <span>{{ idea.user }}</span>
-        <span>{{ idea.text }}</span>
-        <span>{{ idea.channel }}</span>
-        <span>{{ idea.teamId }}</span>
-      </div>
-    </div>
-  </div>
+  <v-container fluid>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 md4 v-for="idea in ideas" :key="idea._id">
+        <v-card>
+          <v-layout row align-center spacer>
+            <v-flex xs2 sm2 md2>
+              <v-avatar
+                  slot="activator"
+                  size="36px"
+                >
+                  <img
+                    src="https://avatars0.githubusercontent.com/u/9064066?v=4&s=460"
+                    alt="Avatar"
+                  >
+              </v-avatar>
+            </v-flex>
 
+            <v-flex sm5 md3>
+              <v-card-title>{{ idea.user }}</v-card-title>
+            </v-flex>
+          </v-layout>
+
+          <div style="text-align: start; padding: 16px;">
+            <div>{{ idea.text }}</div>
+            <div>{{ idea.channel }}</div>
+            <div>{{ idea.teamId }}</div>
+          </div>
+
+          <v-card-actions>
+            <v-btn flat color="blue">share</v-btn>
+            <v-btn flat color="green">endorse</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-  import IdeaServices from '@/services/IdeaServices'
-  export default {
-    name: 'ideas',
-    data() {
-      return {
-        ideas: []
-      }
-    },
-    mounted() {
-      this.getIdeas()
-    },
-    methods: {
-      async getIdeas() {
-        const response = await IdeaServices.fetchIdeas()
-        this.ideas = response.data
-      }
+import IdeaServices from "@/services/IdeaServices";
+export default {
+  name: "ideas",
+  data() {
+    return {
+      ideas: []
+    };
+  },
+  mounted() {
+    this.getIdeas();
+  },
+  methods: {
+    async getIdeas() {
+      const response = await IdeaServices.fetchIdeas();
+      this.ideas = response.data;
     }
   }
+};
 </script>
-<style>
-  .ideas {
-    display: flex;
-    flex-direction: row;
-  }
-</style>
