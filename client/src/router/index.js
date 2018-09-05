12b8f24502b590f25a0f4cde26a02b8e348559a6
Nick Sarafa <nicksarafa@gmail.com>
2018-09-05 10:15:58 +0800

    General settings and Billing routes init

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Ideas from '@/components/Ideas';
+import Billing from '@/components/Billing';
+import Settings from '@/components/Settings';
 
 Vue.use(Router);
 
@@ -11,6 +13,16 @@ export default new Router({
       path: '/',
       name: 'Ideas',
       component: Ideas,
+    },
+    {
+      path: '/Settings',
+      name: 'Settings',
+      component: Settings,
+    },
+    {
+      path: '/Billing',
+      name: 'Billing',
+      component: Billing,
     },
   ],
 });
