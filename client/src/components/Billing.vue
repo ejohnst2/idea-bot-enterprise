3b6cf3ccb23cacbb49b335a0962e242294127205
Nick Sarafa <nicksarafa@gmail.com>
2018-09-06 17:47:29 +0800

    monthly annual button switch init

--- client/src/components/Billing.vue
+++ client/src/components/Billing.vue
@@ -1,22 +1,21 @@
 <template>
   <div>
-    <v-btn v-if="this.isMonthly === true" v-on:click="setPlan(true)" color="indigo" large>Monthly</v-btn>
-    <v-btn v-if="this.isMonthly === false" v-on:click="setPlan(true)" outline color="indigo" large>Monthly</v-btn>
-    <v-btn v-if="this.isMonthly === true" v-on:click="setPlan(false)" outline color="indigo" large>Annual</v-btn>
-    <v-btn v-if="this.isMonthly === false" v-on:click="setPlan(false)" color="indigo" large>Annual</v-btn>
+    <v-btn v-if="this.isMonthly === true" @click="setPlan(true)" color="indigo" large>Monthly</v-btn>
+    <v-btn v-if="this.isMonthly === false" @click="setPlan(true)" outline color="indigo" large>Monthly</v-btn>
+    <v-btn v-if="this.isMonthly === true" @click="setPlan(false)" outline color="indigo" large>Annual</v-btn>
+    <v-btn v-if="this.isMonthly === false" @click="setPlan(false)" color="indigo" large>Annual</v-btn>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      isMonthly: true
+      isMonthly: false
     }
   },
   methods: {
-    setPlan(bool) {
-      console.log(this.isMonthly)
-      return this.isMonthly === bool
+    setPlan() {
+      this.isMonthly = !this.isMonthly
     }
   }
 };
