758b16627b272f2b58f81fef2660b1a1566e0b95
Nick Sarafa <nicksarafa@gmail.com>
2018-09-06 17:35:53 +0800

    monthly and annual billing btns init

--- client/src/components/Billing.vue
+++ client/src/components/Billing.vue
@@ -1,10 +1,23 @@
 <template>
-  <div>Billing</div>
+  <div>
+    <v-btn v-if="this.isMonthly === true" v-on:click="setPlan(true)" color="indigo" large>Monthly</v-btn>
+    <v-btn v-if="this.isMonthly === false" v-on:click="setPlan(true)" outline color="indigo" large>Monthly</v-btn>
+    <v-btn v-if="this.isMonthly === true" v-on:click="setPlan(false)" outline color="indigo" large>Annual</v-btn>
+    <v-btn v-if="this.isMonthly === false" v-on:click="setPlan(false)" color="indigo" large>Annual</v-btn>
+  </div>
 </template>
 <script>
 export default {
-  mounted() {
-    console.log('billing')
+  data() {
+    return {
+      isMonthly: true
+    }
+  },
+  methods: {
+    setPlan(bool) {
+      console.log(this.isMonthly)
+      return this.isMonthly === bool
+    }
   }
 };
 </script>
