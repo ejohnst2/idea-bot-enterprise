9bafa3d90f8ee846d83471dd0ec31a8a1d44d824
Nick Sarafa <nicksarafa@gmail.com>
2018-09-06 18:14:44 +0800

    free card init

--- client/src/components/Billing.vue
+++ client/src/components/Billing.vue
@@ -1,16 +1,35 @@
 <template>
-  <div>
-    <v-btn v-if="this.isMonthly === true" @click="setPlan(true)" color="indigo" large>Monthly</v-btn>
-    <v-btn v-if="this.isMonthly === false" @click="setPlan(true)" outline color="indigo" large>Monthly</v-btn>
-    <v-btn v-if="this.isMonthly === true" @click="setPlan(false)" outline color="indigo" large>Annual</v-btn>
-    <v-btn v-if="this.isMonthly === false" @click="setPlan(false)" color="indigo" large>Annual</v-btn>
-  </div>
+  <v-container>
+    <v-layout wrap align-center>
+      <v-flex xs12 lg12>
+        <v-btn v-if="this.isMonthly === true" @click="setPlan(true)" color="indigo" large>Monthly</v-btn>
+        <v-btn v-if="this.isMonthly === false" @click="setPlan(true)" outline color="indigo" large>Monthly</v-btn>
+        <v-btn v-if="this.isMonthly === true" @click="setPlan(false)" outline color="indigo" large>Annual</v-btn>
+        <v-btn v-if="this.isMonthly === false" @click="setPlan(false)" color="indigo" large>Annual</v-btn>
+      </v-flex>
+      <v-flex xs12 sm6 md4 offset-md2>
+        <div>Free</div>
+      </v-flex>
+      <v-flex xs12 sm6 md4>
+        <div>Super Ideators</div>
+        <v-select
+          :items="options"
+          label="Number of respondents"
+          solo
+        ></v-select>
+      </v-flex>
+
+    </v-layout>
+  </v-container>
 </template>
 <script>
 export default {
   data() {
     return {
-      isMonthly: false
+      isMonthly: false,
+      count: 3,
+      options: [3, 5, 8, 13, 21, 34],
+      price: 7
     }
   },
   methods: {
