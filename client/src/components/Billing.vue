7ccbe4bf468d100f6830d610f775d3dfd4935b1a
Nick Sarafa <nicksarafa@gmail.com>
2018-09-08 10:18:15 +0800

    count of seats and price per seat init

--- client/src/components/Billing.vue
+++ client/src/components/Billing.vue
@@ -7,18 +7,20 @@
         <v-btn v-if="this.isMonthly === true" @click="setPlan(false)" outline color="indigo" large>Annual</v-btn>
         <v-btn v-if="this.isMonthly === false" @click="setPlan(false)" color="indigo" large>Annual</v-btn>
       </v-flex>
+    </v-layout>
+    <v-layout>
       <v-flex xs12 sm6 md4 offset-md2>
         <div>Free</div>
       </v-flex>
       <v-flex xs12 sm6 md4>
         <div>Super Ideators</div>
+        <div>{{ '$' + this.calculatedPrice + ' ' + 'per month' }}</div>
         <v-select
           :items="options"
-          label="Number of respondents"
+          label="Number of seats"
           solo
         ></v-select>
       </v-flex>
-
     </v-layout>
   </v-container>
 </template>
@@ -27,14 +29,19 @@ export default {
   data() {
     return {
       isMonthly: false,
-      count: 3,
+      countOfSeats: 3,
       options: [3, 5, 8, 13, 21, 34],
-      price: 7
+      pricePerMonth: 7,
     }
   },
   methods: {
     setPlan() {
       this.isMonthly = !this.isMonthly
+    }
+  },
+  computed: {
+    calculatedPrice: function() {
+      return this.pricePerMonth * this.countOfSeats
     }
   }
 };
