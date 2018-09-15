6723c9f6e3b9e43bb610f290faa64b0195533de3
Nick Sarafa <nicksarafa@gmail.com>
2018-09-15 09:54:38 +0800

    passport session init

--- client/src/main.js
+++ client/src/main.js
@@ -2,12 +2,19 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import 'vuetify/dist/vuetify.min.css';
 import Vuetify from 'vuetify';
+import axios from 'axios';
 
 import Vue from 'vue';
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+/**
+ * @desc override axios default config to pass cookie session to vue instance on requests
+ * @see https://github.com/expressjs/session/issues/464#issuecomment-306044124
+ */
+axios.defaults.withCredentials = true;
 
 Vue.use(Vuetify);
 
--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default() => {
   return axios.create({
-    baseURL: 'https://idea-machine-enterprise.herokuapp.com',
+    baseURL: 'https://idea-machine-enterprise.herokuapp.com', // || 'http://localhost:3000'
     json: true,
   });
 };
