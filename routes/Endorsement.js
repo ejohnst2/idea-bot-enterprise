c09080bff579ebd8b9bfe1351a66b1d5e24e1fd4
Nick Sarafa <nicksarafa@gmail.com>
2018-09-25 13:31:41 +0800

    prettify all the things

--- routes/Endorsement.js
+++ routes/Endorsement.js
@@ -1,23 +1,23 @@
-const mongoose = require("mongoose");
-const Endorsement = require("../models/Endorsement");
-const IdeaSchema = require("../models/Idea");
+const mongoose = require('mongoose')
+const Endorsement = require('../models/Endorsement')
+const IdeaSchema = require('../models/Idea')
 
 function postSlackEndorsement(req, res) {
-  let myRegexp = /\n\n (.*)/;
-  let match = myRegexp.exec(req.message.text)[1];
+  let myRegexp = /\n\n (.*)/
+  let match = myRegexp.exec(req.message.text)[1]
 
   // query the DB to find the idea that the text is referencing
 
   IdeaSchema.findOne({ text: match }, function(err, idea) {
     let newEndorsement = new Endorsement({
       user: req.user.id,
-      idea_id: idea
-    });
+      idea_id: idea,
+    })
 
-    newEndorsement.save();
-  });
+    newEndorsement.save()
+  })
 }
 
 module.exports = {
-  postSlackEndorsement
-};
+  postSlackEndorsement,
+}
